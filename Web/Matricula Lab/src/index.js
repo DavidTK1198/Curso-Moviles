52c2ad11c205d2bf0a4ce383476d6a77a734392a
Esteban Ugalde <estalonso506@gmail.com>
2022-04-20 09:08:16 -0600

    Algunos avances en frontend

--- Web/Matricula Lab/src/index.js
+++ Web/Matricula Lab/src/index.js
@@ -7,6 +7,7 @@ Contact me via: "arnoldgq612@gmail.com".
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes/Routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
     /*
     ReactDOM.render(...) renders different SFR website's modules using "Routes" component.
--- Web/Matricula Lab/src/pages/Courses/Courses.jsx
+++ Web/Matricula Lab/src/pages/Courses/Courses.jsx
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import '../../css/Courses.css'
 import axios from 'axios';
 import { MDBDataTable,  } from 'mdbreact';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import AddCourseModal from './Components/AddCourseModal';
+import GenericModal from '../../components/GenericModal';
 export default class Courses extends Component {
     constructor(props){
         super(props);
         this.state = {
-            courses: []
+            courses: [],
+            show: false,
+            showDel: false,
+            delID: ""
         }
         this.tabledata = this.tabledata.bind(this);
         this.openModal = this.openModal.bind(this);
         this.closeModal = this.closeModal.bind(this);
-        this.updateCoursesSort = this.updateCoursesSort.bind(this);
+        this.refreshPage = this.refreshPage.bind(this);
     }
     openModal = () => {
       this.setState({ show: true });
@@ -20,10 +27,16 @@ export default class Courses extends Component {
     closeModal = () => {
       this.setState({ show: false });
     };
+    openModalDel(id) {
+      this.setState({ showDel: true, delID: id });
+  };
+    closeModalDel = () => {
+      this.setState({ showDel: false });
+    };
     componentDidMount() {
-      this.updateCoursesSort();
+      this.refreshPage();
   }
-  updateCoursesSort() {
+  refreshPage() {
     let query = new URLSearchParams(this.props.location.search);
     console.log(query.get('codigo'))
     let options = {
@@ -44,6 +57,33 @@ export default class Courses extends Component {
       console.log(error);
       });
 };
+  deleteCourse(idCourse){
+    let options = {
+      url: "",
+      method: "DELETE",
+      header: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+      }
+  }
+  axios(options)
+      .then(response => {
+          this.refreshPage();
+          toast.success("El curso fue eliminado correctamente!", {
+              position: toast.POSITION.TOP_RIGHT,
+              pauseOnHover: true,
+              theme: 'colored',
+              autoClose: 5000
+          });
+      }).catch(error => {
+          toast.error("Error al remover el curso.", {
+              position: toast.POSITION.TOP_RIGHT,
+              pauseOnHover: true,
+              theme: 'colored',
+              autoClose: 5000
+          });
+      });
+  }
     tabledata() {
            let data = {
             columns: [
@@ -68,28 +108,55 @@ export default class Courses extends Component {
                 label: 'Nombre',
                 field: 'nombre',
                 sort:  'asc',
-              }
-              
+              },
+              {
+                label: 'Eliminar',
+                field: 'delete',
+                sort:  'asc',
+              }            
             ],
             rows: this.state.courses   
             }    
             for(let i in data.rows){
               let cName = data.rows[i]['nombre'];
-              data.rows[i]['nombre'] = <Link to={{ pathname: "/curso", search: `?id=${data.rows[i]['id']}` }}>
-              {cName}</Link> 
-            }       
+              data.rows[i]['nombre'] = 
+              <Link 
+                to={{ pathname: "/grupos", search: `?codigo=${data.rows[i]['codigo']}` }}>
+                {cName}
+              </Link> 
+            }      
+            for(let i in data.rows){
+              data.rows[i]['delete'] = 
+              <Button variant="secondary" onClick={() => this.openModalDel(data.rows[i]['codigo'])}>
+                Eliminar
+              </Button> 
+            }   
             return data
     }   
     render() {
         return (
-            <div>           
+            <div>     
+              <Button size="sm" onClick={this.openModal} variant="success" key="AddButton">
+                <i className="bi bi-plus-square"></i> {' '}
+                Agregar curso
+              </Button>       
               <MDBDataTable 
                 searchLabel='Buscar'
-                //autoWidth={true}
                 responsive
                 hover={true}
                 data={this.tabledata()}              
                 />
+              <AddCourseModal
+                show = {this.state.show}
+                closeModal = {this.closeModal}
+              />
+              <GenericModal
+                show={this.state.showDel}
+                close={this.closeModalDel}
+                action={this.deleteCourse}
+                header={"Eliminar curso"}
+                body={"¿Esta seguro que desea eliminar este curso?"} />
+              <ToastContainer />
             </div>
         );
     }
